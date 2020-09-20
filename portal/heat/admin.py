fdd0260154dfe9810f81f92c4e1252e309e4b350
Stepan Skriabin <stepan.skrjabin@gmail.com>
2020-09-20 11:09:12 +0300

    FEAT: added "heat" in admin panels and settings.py

--- portal/heat/admin.py
+++ portal/heat/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Home
+from .models import HomeApartment
+from .models import HomeParametrs
+from .models import ConsumptionODPU
+from .models import ConsumptionIPU
+from .models import ConsumptionRSO
+from .models import ResourceCharge
 
-# Register your models here.
+
+admin.site.register(Home)
+admin.site.register(HomeApartment)
+admin.site.register(HomeParametrs)
+admin.site.register(ConsumptionODPU)
+admin.site.register(ConsumptionIPU)
+admin.site.register(ConsumptionRSO)
+admin.site.register(ResourceCharge)
