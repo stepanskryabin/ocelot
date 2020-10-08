1f367b84828df31ee06926d612a696ff29406a4c
Stepan Skriabin <stepan.skrjabin@gmail.com>
2020-10-09 01:36:36 +0300

    FIX: исправлены шаблоны

--- portal/portal/settings.py
+++ portal/portal/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
-from os import path
 from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
