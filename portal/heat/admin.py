776aecbfd290a816f80e5a41542c8a1794c555ac
Stepan Skriabin <stepan.skrjabin@gmail.com>
2020-11-01 19:19:17 +0300

    Обновлены названия таблиц для панели администратора
    дополнительно проведён рефакторинг кода

--- portal/heat/admin.py
+++ portal/heat/admin.py
@@ -1,69 +1,195 @@
 from django.contrib import admin
 
-from .models import ConsumptionODPU
-from .models import ConsumptionApartmentIPU
-from .models import ConsumptionHomeIPU
-from .models import ConsumptionRSO
-from .models import ResourceCharge
+from .models import Odpu
+from .models import ApartmentIpu
+from .models import HomeIpu
+from .models import Rso
+from .models import ResourceOdn
+from .models import ResourceChargeOdn
 
 
-@admin.register(ConsumptionODPU)
-class ConsumptionODPUView(admin.ModelAdmin):
-    list_display = ("home", "resource_type", "time_record", "Q1", "M1", "M2")
-    list_filter = ("home", "resource_type", "time_record", "Q1", "M1", "M2")
-    search_fields = ("home", "resource_type", "time_record", "Q1", "M1", "M2")
+@admin.register(Odpu)
+class OdpuView(admin.ModelAdmin):
+    list_display = ("home",
+                    "resource_type",
+                    "time_record",
+                    "temp1",
+                    "temp2",
+                    "temp3",
+                    "g1",
+                    "g2",
+                    "work_time",
+                    "idle_time",
+                    "q1",
+                    "q2",
+                    "m1",
+                    "m2",
+                    "m3",
+                    "m")
+    list_filter = ("home",
+                   "resource_type",
+                   "time_record",
+                   "m")
+    search_fields = ("home",
+                     "resource_type",
+                     "time_record",
+                     "m")
     date_hierarchy = ("time_record")
-    ordering = ("home", "resource_type", "time_record", "Q1", "M1", "M2")
+    ordering = ("home",
+                "resource_type",
+                "time_record")
 
 
-@admin.register(ConsumptionApartmentIPU)
-class ConsumptionApartmentIPUView(admin.ModelAdmin):
-    list_display = ("home", "time_record", "apartment", "resource_type",
-                    "IPU_indication", "IPU_consumption", "IPU_calories",
-                    "recalculation_mass", "recalculation_calories")
-    list_filter = ("home", "time_record", "apartment", "resource_type")
-    search_fields = ("home", "time_record", "apartment", "resource_type")
+@admin.register(ApartmentIpu)
+class ApartmentIpuView(admin.ModelAdmin):
+    list_display = ("home",
+                    "time_record",
+                    "apartment",
+                    "resource_type",
+                    "indication",
+                    "consumption",
+                    "calories",
+                    "recalc_mass",
+                    "recalc_calories")
+    list_filter = ("home",
+                   "time_record",
+                   "apartment",
+                   "resource_type",
+                   "indication",
+                   "consuption")
+    search_fields = ("home",
+                     "time_record",
+                     "apartment",
+                     "resource_type")
     date_hierarchy = ("time_record")
-    ordering = ("home", "time_record", "apartment", "resource_type")
+    ordering = ("home",
+                "time_record",
+                "apartment",
+                "resource_type")
 
 
-@admin.register(ConsumptionHomeIPU)
-class ConsumptionHomeIPUView(admin.ModelAdmin):
-    list_display = ("home", "time_record", "resource_type",
-                    "IPU_mass", "IPU_calories")
-    list_filter = ("home", "time_record", "resource_type")
-    search_fields = ("home", "time_record", "resource_type")
+@admin.register(HomeIpu)
+class HomeIpuView(admin.ModelAdmin):
+    list_display = ("home",
+                    "time_record",
+                    "resource_type",
+                    "mass",
+                    "calories")
+    list_filter = ("home",
+                   "time_record",
+                   "resource_type",
+                   "mass")
+    search_fields = ("home",
+                     "time_record",
+                     "resource_type")
     date_hierarchy = ("time_record")
-    ordering = ("home", "time_record", "resource_type")
+    ordering = ("home",
+                "time_record",
+                "resource_type")
 
 
-@admin.register(ConsumptionRSO)
-class ConsumptionRSOView(admin.ModelAdmin):
-    list_display = ("home", "time_record", "ODPU_mass", "IPU_RP_mass",
-                    "IPU_nonRP_mass", "ODN_mass", "negative_ODN_mass",
-                    "result_ODN_mass", "payable_ODN_mass")
-    list_filter = ("home", "time_record", "ODPU_mass", "IPU_RP_mass",
-                   "IPU_nonRP_mass", "ODN_mass", "negative_ODN_mass",
-                   "result_ODN_mass", "payable_ODN_mass")
-    search_fields = ("home", "time_record", "ODPU_mass", "IPU_RP_mass",
-                     "IPU_nonRP_mass", "ODN_mass", "negative_ODN_mass",
-                     "result_ODN_mass", "payable_ODN_mass")
+@admin.register(Rso)
+class RsoView(admin.ModelAdmin):
+    list_display = ("home",
+                    "time_record",
+                    "ODPU_mass",
+                    "ODPU_calories",
+                    "IPU_RP_mass",
+                    "IPU_RP_mass",
+                    "IPU_RP_calories",
+                    "IPU_nonRP_mass",
+                    "IPU_nonRP_calories",
+                    "ODN_mass",
+                    "ODN_calories",
+                    "negative_ODN_mass",
+                    "negative_ODN_calories",
+                    "result_ODN_mass",
+                    "result_ODN_calories",
+                    "payable_ODN_mass",
+                    "payable_ODN_calories")
+    list_filter = ("home",
+                   "time_record",
+                   "ODPU_mass",
+                   "IPU_RP_mass",
+                   "IPU_nonRP_mass",
+                   "ODN_mass",
+                   "negative_ODN_mass",
+                   "result_ODN_mass",
+                   "payable_ODN_mass")
+    search_fields = ("home",
+                     "time_record",
+                     "ODPU_mass",
+                     "IPU_RP_mass",
+                     "IPU_nonRP_mass",
+                     "ODN_mass",
+                     "negative_ODN_mass",
+                     "result_ODN_mass",
+                     "payable_ODN_mass")
     date_hierarchy = ("time_record")
-    ordering = ("home", "time_record", "ODPU_mass", "IPU_RP_mass",
-                "IPU_nonRP_mass", "ODN_mass", "negative_ODN_mass",
-                "result_ODN_mass", "payable_ODN_mass")
+    ordering = ("home",
+                "time_record",
+                "ODPU_mass",
+                "IPU_RP_mass",
+                "IPU_nonRP_mass",
+                "ODN_mass",
+                "negative_ODN_mass",
+                "result_ODN_mass",
+                "payable_ODN_mass")
 
 
-@admin.register(ResourceCharge)
-class ResourceChargeView(admin.ModelAdmin):
-    list_display = ("home", "time_record", "norm_mass", "norm_finance",
-                    "accrued_mass", "accured_finance", "saldo_mass",
-                    "saldo_finance", "payed", "tarif", "nalog")
-    list_filter = ("home", "time_record", "norm_mass", "norm_finance",
-                   "accrued_mass", "accured_finance", "saldo_mass",
-                   "saldo_finance", "payed")
-    search_fields = ("home", "time_record", "norm_mass")
+@admin.register(ResourceOdn)
+class ResourceOdnView(admin.ModelAdmin):
+    list_display = ("home",
+                    "time_record",
+                    "odn_mass",
+                    "odn_gkal")
+    list_filter = ("home",
+                   "time_record",
+                   "odn_mass",
+                   "odn_gkal")
+    search_fields = ("home", "time_record")
     date_hierarchy = ("time_record")
-    ordering = ("home", "time_record", "norm_mass", "norm_finance",
-                "accrued_mass", "accured_finance", "saldo_mass",
-                "saldo_finance", "payed")
+    ordering = ("home",
+                "time_record",
+                "odn_mass",
+                "odn_gkal")
+
+
+@admin.register(ResourceChargeOdn)
+class ResourceChargeOdnView(admin.ModelAdmin):
+    list_display = ("home",
+                    "time_record",
+                    "mass",
+                    "finance",
+                    "payed",
+                    "norm_mass",
+                    "norm_finance",
+                    "over_mass",
+                    "over_finance"
+                    "saldo_mass",
+                    "saldo_finance")
+    list_filter = ("home",
+                   "time_record",
+                   "mass",
+                   "finance",
+                   "payed",
+                   "norm_mass",
+                   "norm_finance",
+                   "over_mass",
+                   "over_finance"
+                   "saldo_mass",
+                   "saldo_finance")
+    search_fields = ("home",
+                     "time_record")
+    date_hierarchy = ("time_record")
+    ordering = ("home",
+                "time_record",
+                "mass",
+                "finance",
+                "payed",
+                "norm_mass",
+                "norm_finance",
+                "over_mass",
+                "over_finance"
+                "saldo_mass",
+                "saldo_finance")
